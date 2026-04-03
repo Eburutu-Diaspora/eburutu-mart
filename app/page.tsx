41cfe976e163027c8c17daca088aa148af94f7fd
Eburutu-Diaspora <eburutumart@gmail.com>
2026-04-03 21:57:00 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,11 @@ import { CategorySection } from '@/components/home/category-section'
 import { FeaturedProducts } from '@/components/home/featured-products'
 import { CommunitySection } from '@/components/home/community-section'
 
-import { CommoditiesTicker } from '@/components/home/commodities-ticker'
-
+import dynamic from 'next/dynamic'
+const CommoditiesTicker = dynamic(
+  () => import('@/components/home/commodities-ticker').then(mod => ({ default: mod.CommoditiesTicker })),
+  { ssr: false }
+)
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-background">
