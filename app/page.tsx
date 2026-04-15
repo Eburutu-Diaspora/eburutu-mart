82fb7ad0aa20e03a634d5f64f6fca5864f3ef261
Eburutu <eburutumart@gmail.com>
2026-04-15 23:43:23 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,16 @@ import { Footer } from '@/components/navigation/footer'
 import { HeroSection } from '@/components/home/hero-section'
 import { CategorySection } from '@/components/home/category-section'
 import { FeaturedProducts } from '@/components/home/featured-products'
+import { NewArrivals } from '@/components/home/new-arrivals'
+import { RecommendedProducts } from '@/components/home/recommended-products'
 import { CommunitySection } from '@/components/home/community-section'
-
 import dynamic from 'next/dynamic'
+
 const CommoditiesTicker = dynamic(
   () => import('@/components/home/commodities-ticker').then(mod => ({ default: mod.CommoditiesTicker })),
   { ssr: false }
 )
+
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-background">
@@ -20,6 +23,8 @@ export default function HomePage() {
         <HeroSection />
         <CategorySection />
         <FeaturedProducts />
+        <NewArrivals />
+        <RecommendedProducts />
         <CommunitySection />
       </main>
       <Footer />
