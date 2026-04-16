6f001d6aa7f1ff5ce5cdbac0d22b59a0e0c0f6c9
Eburutu <eburutumart@gmail.com>
2026-04-17 00:26:41 +0100

    Update related-products.tsx

--- components/products/related-products.tsx
+++ components/products/related-products.tsx
@@ -3,7 +3,7 @@
 
 import { useState, useEffect } from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
+
 import { Card, CardContent } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 import { Badge } from '@/components/ui/badge'
